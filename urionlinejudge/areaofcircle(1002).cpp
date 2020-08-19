23a4b3d7c9081910f1780cd0e8055cefa11cbf7b
kirankumar2079 <60278545+kirankumar2079@users.noreply.github.com>
2020-08-19 18:36:18 +0530

    Create areaofcircle(1002).cpp
    easy one

---
+++ urionlinejudge/areaofcircle(1002).cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double R,A;
+    double n=3.14159;
+    cin>>R;
+    A=n*R*R;
+    printf("A=%.4f",A);
+    return 0;
+}
+//to be noted that a must come after r is taken in
