c3d1b7defbfbb80ef4039c440410723310438d8c
kirankumar2079 <60278545+kirankumar2079@users.noreply.github.com>
2020-09-24 12:35:04 +0530

    Update Average2(1006).py

--- urionlinejudge/Average2(1006).py
+++ urionlinejudge/Average2(1006).py
@@ -1,4 +1,5 @@
 ***
+solved on 24 sep 2020
 Read three values (variables A, B and C), which are the three student's grades. Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5. Consider that each grade can go from 0 to 10.0, always with one decimal place.
 
 Input
