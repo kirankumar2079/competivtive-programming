dedaaace304dcadfc25d3f7cc413f5cf5d64f504
kirankumar <kiran09082001@gmail.com>
2024-04-21 22:24:52 +0530

    pattern 12 completed - 21 APR 2024

---
+++ A-Z DSA/patterns/pattern_12.cpp
@@ -0,0 +1,49 @@
+/* question
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        // number
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j<<" ";
+        }
+        // space
+        for (int j=1 ; j<=2*(n-i);j++)
+        {
+            cout<<" ";
+        }
+        // number
+        for(int j=i ; j>=1 ; j--)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+int main()
+{
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        print(n);
+    }
+    return 0;
+}
