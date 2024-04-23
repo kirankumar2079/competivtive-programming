c2234385dd375366faed8c896f7ec1d9a365e06b
kirankumar <kiran09082001@gmail.com>
2024-04-23 22:56:56 +0530

    STL pairs

---
+++ A-Z DSA/STL/pairs.cpp
@@ -0,0 +1,19 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    pair<int , int> k;
+    int n , m ;
+    cin >> n ;
+    k.first = n;
+    cin >> m ;
+    k.second = m;
+    cout<< k.first << k.second ;
+    return 0;
+}
