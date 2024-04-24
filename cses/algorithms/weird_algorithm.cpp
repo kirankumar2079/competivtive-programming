ca96f86a992d86aad36936ad389c77638b622ed5
kirankumar <kiran09082001@gmail.com>
2024-04-24 21:38:59 +0530

    weird algo with comments - 24 APR 2024

--- cses/algorithms/weird_algorithm.cpp
+++ cses/algorithms/weird_algorithm.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 int main()
 {
+    // input
     long long n;
     cin>>n;
+    // iterate till the given number does not become 1
     while(n!=1)
     {
+        // print the changed value in every iteration
         cout << n << " ";
+        // perform the operation specified
         if (n % 2 == 0)
         {
             n = n /2 ;
@@ -19,5 +23,6 @@ int main()
             n = n*3+1;
         }
     }
+    // print the last value 1
     cout<<"1 ";
 }
